7196d9031e9b09fa02cd3536c1cf7707ff894678
Steven Bouldin <96607187+Sacred-G@users.noreply.github.com>
2024-01-10 10:25:51 -0600

    Update util.py

--- excelchat-streamlit-main/util.py
+++ excelchat-streamlit-main/util.py
@@ -30,14 +30,18 @@ def get_baidu_qianfan_model(client_id, client_secret):
     return LangchainLLM(llm_core)
 
 
+# Function to get a custom prompt template
 def get_prompt_template():
     instruction_template = """
-使用提供的 dataframes ('dfs') 分析这个数据，过程中不要调用 dataframe set_index 对数据排序.
-1. 准备: 如果有必要对数据做预处理和清洗
-2. 执行: 对数据进行数据分析操作 (grouping, filtering, aggregating, etc.)
-3. 分析: 进行实际分析（如果用户要求plot chart，请在代码中添加如下两行代码设置字体, 并将结果保存为图像文件temp_chart.png，并且不显示图表）
-plt.rcParams['font.sans-serif']=['SimHei']
-plt.rcParams['axes.unicode_minus']=False    
+    Using the provided dataframes ('dfs'), analyze this data, and avoid calling dataframe set_index to sort the data during the process.
+    1. Preparation: If necessary, preprocess and clean the data.
+    2. Execution: Perform data analysis operations on the data (grouping, filtering, aggregating, etc.).
+    3. Analysis: Conduct actual analysis (if the user requests a plot chart, please add the following two lines of code in the script to set the font, save and display the result as an image file 'temp_chart.png').
+    plt.rcParams['font.sans-serif']
+    plt.rcParams['axes.unicode_minus']=False
+    4. Use original dataframe ('dfs') for additional questions.
     """
     custom_template = GeneratePythonCodePrompt(custom_instructions=instruction_template)
     return custom_template
+
+# Main function
